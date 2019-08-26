fe000095319c7a3795b98da184a076f246fe04f9
Rzeszutek <rzeszutp@emea.roche.com>
2019-08-26 15:29:45 +0200

    beginning of the cart creation

--- src/context.js
+++ src/context.js
@@ -12,7 +12,10 @@ const ProductContext = React.createContext();
         detailProduct:detailProduct,    
         cart:[],
         modalOpen:false,
-        modalProduct:detailProduct, 
+        modalProduct:detailProduct,
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0 
      };
      componentDidMount(){
          this.setProducts();
@@ -65,7 +68,19 @@ const ProductContext = React.createContext();
      closeModal = () =>{
      this.setState(()=>{
          return{modalOpen:false}
-     })   
+     });   
+     };
+     increment = (id) =>{
+        console.log('this is increment'); 
+     }
+     decrement = (id) =>{
+        console.log('this is decrement'); 
+     }
+     removeItem = (id) => {
+        console.log('item removed');
+     }
+     clearCart = () =>{
+        console.log('cart was cleared')
      }
     render() {
         return (
@@ -75,7 +90,11 @@ const ProductContext = React.createContext();
                     handleDetail:this.handleDetail,
                     addToCart:this.addToCart,
                     openModal:this.openModal,
-                    closeModal:this.closeModal
+                    closeModal:this.closeModal,
+                    increment:this.increment,
+                    decrement:this.decrement,
+                    removeItem:this.removeItem,
+                    clearCart:this.clearCart
                 }}
             >
              {this.props.children}   
