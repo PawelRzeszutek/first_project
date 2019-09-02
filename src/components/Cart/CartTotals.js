e291957a7614339522fb4503f7b009a6124be44f
Rzeszutek <rzeszutp@emea.roche.com>
2019-09-02 11:45:51 +0200

    CartTotals component ending

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 export default function CartTotals({value}) {
-    const{ cartSubtotal, cartTax, cartTotal, clearCart } = value;
+    const{ cartSubTotal, cartTax, cartTotal, clearCart } = value;
     return (<React.Fragment>
         <div className="container">
             <div className="row">
@@ -10,7 +10,22 @@ export default function CartTotals({value}) {
                         <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
                         clear cart
                         </button>    
-                    </Link>    
+                    </Link> 
+                    <h5>
+                        <span className="text-title">
+                        subtotal :</span>
+                        <strong>$ {cartSubTotal}</strong>
+                    </h5>
+                    <h5>
+                        <span className="text-title">
+                        tax :</span>
+                        <strong>$ {cartTax}</strong>
+                    </h5> 
+                    <h5>
+                        <span className="text-title">
+                        total :</span>
+                        <strong>$ {cartTotal}</strong>
+                    </h5>    
                 </div>    
             </div>
         </div>
