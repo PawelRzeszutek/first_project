e539d4ac93b54efbaf7b54b15f9e00b5fba41095
Rzeszutek <rzeszutp@emea.roche.com>
2019-09-02 15:27:12 +0200

    Function: removing single item

--- src/context.js
+++ src/context.js
@@ -77,7 +77,23 @@ const ProductContext = React.createContext();
         console.log('this is decrement'); 
      };
      removeItem = (id) => {
-        console.log('item removed');
+        let tempProducts = [...this.state.products];   
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(()=>{
+            return {
+                cart:[...tempCart],
+                products:[...tempProducts]
+            }    
+        },()=>{
+            this.addTotals();
+        })
      };
      clearCart = () =>{
         this.setState(()=>{
