a0a837fa86304d762f91ef621883df08f83a70ee
Rzeszutek <rzeszutp@emea.roche.com>
2019-09-03 11:11:08 +0200

    Function: decrement

--- src/context.js
+++ src/context.js
@@ -90,7 +90,27 @@ const ProductContext = React.createContext();
         );
      };
      decrement = (id) =>{
-        console.log('this is decrement'); 
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];    
+        product.count = product.count - 1;
+        
+        if(product.count === 0) {
+            this.removeItem(id)
+        }
+        else {
+            product.total = product.count * product.price;
+            this.setState(
+                () => {
+                    return { cart: [...tempCart] };
+                },
+                () => {
+                    this.addTotals();
+                }
+            );
+        }
      };
      removeItem = (id) => {
         let tempProducts = [...this.state.products];   
